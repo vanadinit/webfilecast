5848b695c231fdd1d66f06c9990fe773dc7dc621
Johannes Paul <vanadinit@quantentunnel.de>
2023-10-07 09:26:09 +0200

    Add dependencies, CHANGELOG, some basic files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='webfilecast',
+    version='1.0.0',
+    description='Webfrontend to cast local videos to your chromecast',
+    keywords=['Chromecast', 'video', 'local', 'movie', 'terminalcast'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/vanadinit/webfilecast',
+    author='Johannes Paul',
+    author_email='vanadinit@quantentunnel.de',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'terminalcast',
+        'flask',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
